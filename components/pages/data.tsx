d911724efe330af691ad2acf2d28fb13cf10a22d
Ryan <ryan@geekful.dev>
2022-06-27 08:55:39 +0800

    feat: use PaperWithHeading on start pages

--- components/pages/data.tsx
+++ components/pages/data.tsx
@@ -1,4 +1,8 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import React from 'react';
+import PaperWithHeading from '../paper-with-heading';
 
 interface Props {
   formsToCreate: Array<String>;
@@ -6,12 +10,13 @@ interface Props {
 
 function StartDataPage({ formsToCreate }: Props) {
   return (
-    <div>
-      StartDataPage{' '}
-      {formsToCreate.map((f) => (
-        <p>{f}</p>
-      ))}
-    </div>
+    <Container maxWidth="md">
+      <PaperWithHeading>
+        <div>
+          <h1>Hello</h1>
+        </div>
+      </PaperWithHeading>
+    </Container>
   );
 }
 
